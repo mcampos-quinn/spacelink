1fda4f5cee18ed65891fb436faca79015c14d6f7
mcampos-quinn <mcq@berkeley.edu>
2022-11-29 10:37:41 -0800

    add note about cspace xpath to sample config file

--- sample_config.py
+++ sample_config.py
@@ -7,7 +7,12 @@ CSPACE_INSTANCE = {
 	"art_collection": {
 		"resource type":"13,
 		"fields_to_extract":{
-			"rs field id":"cspace field tag name"
+			"rs field id":"cspace field tag name",
+			# ~ ~ ~ NOTE!! this  might also need to be an XPath expression to get
+			# at a nested tag. in the below example the cspace <dateDisplayDate> tag can exist 
+			# under any number of parent tag groups, so we have to specify which
+			# one we actually want to retrieve
+			"12":"objectProductionDateGroup//dateDisplayDate"
 		},
 		"query results per page":10,
 		"cspace max query results":12,
